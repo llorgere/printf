0daee814fe8f56b5c90870052d6ea1a0ce62ac95
Louis-Ubuntu <louis.lorgere@noos.fr>
2017-10-29 20:22:27 +0100

    4

---
+++ libftprintf.h
@@ -0,0 +1,14 @@
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+
+typedef struct		s_list
+{
+}					flag_list;
+
